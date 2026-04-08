d6650c29fff955f5195f9292dd77b069deb80ab0
hzy <1754203003@qq.com>
2026-04-08 21:58:11 +0800

    core

--- src/init/task_exec.rs
+++ src/init/task_exec.rs
@@ -1,42 +1,62 @@
+﻿use anyhow::{anyhow, Context, Result};
 use tokio::sync::mpsc::Sender;
-use tracing::{info, warn};
+use tracing::info;
+
 use crate::device::Device;
 use crate::func::FunctionWorker;
 use crate::web::WebMessage;
-use anyhow::Result;
 
 #[derive(Debug)]
-pub struct TaskExecutor{
+pub struct TaskExecutor {
     sender: Sender<WebMessage>,
 }
 
 impl TaskExecutor {
     pub fn new(sender: Sender<WebMessage>) -> TaskExecutor {
-        TaskExecutor{
-            sender,
-        }
+        TaskExecutor { sender }
     }
+
     pub fn get_sender(&self) -> Sender<WebMessage> {
         self.sender.clone()
     }
 }
 
-
-pub async fn execute(sender: Sender<WebMessage>,device:Option<Device>,func:Option<FunctionWorker>)->Result<()> {
-
+pub fn execute_sync(device: Option<Device>, func: Option<FunctionWorker>) -> Result<WebMessage> {
     let mut device = device.unwrap_or(Device::None);
-    if let Some(func_worker) = func {
-        let FunctionWorker{
-            func_id,
-            mut args,
-            mut func,
-        } = func_worker;
-        info!("{func_id}({args})  is running",func_id = func_id,args = args.join(" "));
-        let result =func(&mut args, &mut device);
-        let _ = sender.send(result).await;
-        info!("{} has finished execution",func_id);
-        return Ok(());
-    }
-    warn!("Unhandled function, Nothing execute ...");
+    let func_worker = func.ok_or_else(|| anyhow!("function not found"))?;
+
+    let FunctionWorker {
+        func_id,
+        mut args,
+        mut func,
+    } = func_worker;
+
+    info!(
+        "{func_id}({args}) is running",
+        func_id = func_id,
+        args = args.join(" ")
+    );
+
+    let result = func(&mut args, &mut device);
+
+    info!("{} has finished execution", func_id);
+    Ok(result)
+}
+
+pub async fn execute(
+    sender: Sender<WebMessage>,
+    device: Option<Device>,
+    func: Option<FunctionWorker>,
+) -> Result<()> {
+    let result = tokio::task::spawn_blocking(move || execute_sync(device, func))
+        .await
+        .context("blocking task join failed")??;
+
+    sender
+        .send(result)
+        .await
+        .context("failed to send web message")?;
+
+    info!("task result sent to web channel");
     Ok(())
 }
--- src/init/task_listen.rs
+++ src/init/task_listen.rs
@@ -1,49 +1,47 @@
-use log::info;
-use crate::init::{TaskDispatcher, TaskExecutor};
+﻿use log::info;
 use tokio::sync::mpsc::Receiver;
 use tracing::error;
-use crate::source::Event;
-use crate::init::task_exec::execute;
 
-pub struct TaskListener{
+use crate::init::{TaskDispatcher, TaskExecutor};
+use crate::init::task_exec::execute;
+use crate::source::Event;
+
+pub struct TaskListener {
     executor: TaskExecutor,
     listener: Receiver<Event>,
     dispatcher: TaskDispatcher,
-    
 }
 
-
-
 impl TaskListener {
-    pub fn new(executor: TaskExecutor, listener: Receiver<Event>,dispatcher:TaskDispatcher) -> Self {
-        TaskListener{
+    pub fn new(executor: TaskExecutor, listener: Receiver<Event>, dispatcher: TaskDispatcher) -> Self {
+        TaskListener {
             executor,
             listener,
             dispatcher,
         }
     }
 
-    
     // only do that receive message and then 'inform'
-    pub async fn run(mut self){
+    pub async fn run(mut self) {
         loop {
             match self.listener.recv().await {
                 Some(event) => {
                     info!("[TaskListener] received event: {:?}", event);
-                    let device =self.dispatcher.find_device(&event);
-                    let func =self.dispatcher.find_func(&event);
+                    let device = self.dispatcher.find_device(&event);
+                    let func = self.dispatcher.find_func(&event);
                     let sender = self.executor.get_sender();
-                    tokio::spawn(async {
-                        match execute(sender,device,func).await{
+
+                    tokio::spawn(async move {
+                        match execute(sender, device, func).await {
                             Ok(_) => {
                                 info!("[TaskListener] executed successfully");
-                            },
-                            Err(e) =>{
+                            }
+                            Err(e) => {
                                 error!("[TaskListener] executed failed {:?}", e);
                             }
                         }
                     });
-                },
+                }
                 None => {
                     info!("[TaskListener] received None event");
                     break;
@@ -51,7 +49,4 @@ impl TaskListener {
             }
         }
     }
-
-
 }
-
